db32e6ee296938766cbfa85c105ca819e32cb15a
romain.loubeyre <romain.loubeyre@epsi.fr>
2021-03-03 17:08:22 +0100

    git_ignore

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const connect = require("./Node.js/Postgres/connect.js");
+const QRCode = require('qrcode');
+ 
+// QRCode.toDataURL('Romain', function (err, url) {
+//   console.log(url)
+// })
 
-client = connect.connection();
+// client = connect.connection();
 
-client.query('SELECT * FROM customer', (err, res) => {
-    console.log(res)
-    client.end()
-})
+// client.query('SELECT * FROM customer', (err, res) => {
+//     console.log(res)
+//     client.end()
+// })
