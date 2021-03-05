ee4d28f6a0e3a2cd63ac6921d74e75839b67ce80
romain.loubeyre <romain.loubeyre@epsi.fr>
2021-03-05 09:21:55 +0100

    React

--- Node.js/Entities/customer.js
+++ Node.js/Entities/customer.js
@@ -3,7 +3,7 @@ class customer {
     {
 
     }
-    constructor(id, first_name, last_name, mail, password, birth_date, creation_date)
+    constructor(id, first_name, last_name, mail, password, birth_date, creation_date, id_type)
     {
         this.id = id;
         this.first_name = first_name;
@@ -12,5 +12,6 @@ class customer {
         this.password = password;
         this.birth_date = birth_date;
         this.creation_date = creation_date;
+        this.id_type = id_type;
     }
 }
