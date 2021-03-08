8f1943ecfa167aa7ee06fa6c01c3398fd9a97db6
joackim.perochaud <joackim.perochaud@epsi.fr>
2021-03-08 17:46:58 +0100

    couponQueries

---
+++ Node.js/queries/couponQueries.js
@@ -0,0 +1,25 @@
+//import
+import db from "../config/Postgres/db.js"
+
+// Recuperation d'un coupon
+export const GetCouponById = (request, response) => {
+    const id = request.params.id;
+    db('SELECT * FROM  coupon  where id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+// Recuperation d' une liste de  coupon
+export const GetlistCouponById = (request, response) => {
+    const id = request.params.id;
+    db('SELECT * FROM coupon ORDER BY id ASC', [] ,(error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+
+}
